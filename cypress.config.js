94b3fd7af87787fd002210fceafb0f9bf302b839
paul <priyanshinair.work@gmail.com>
2024-11-30 12:18:45 +0530

    push

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'reporters/custom.js',
   e2e: {
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
 });
